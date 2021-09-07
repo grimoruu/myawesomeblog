35077b5dfef99fe58682f52aa392d2b47c4f57b0
Ainaz <grimoruu@yandex.ru>
2021-09-07 15:25:03 +0300

    add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
